7f519365b619708524e60b11e601724e608b9d3b
FireFlame74 <sandro.figo@gmx.at>
2018-05-12 18:49:50 +0200

    Added editor script

---
+++ Assets/Editor/RopeEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Rope))]
+public class RopeEditor : Editor {
+
+    Rope rope;
+
+    public override void OnInspectorGUI()
+    {
+        rope = (Rope)target;
+
+        base.OnInspectorGUI();
+    }
+
+    private void OnSceneGUI()
+    {
+        
+    }
+}
